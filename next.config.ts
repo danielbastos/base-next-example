b6477b0354000dec768a3b9a35fc7ef97f82fd04
Daniel Bastos <danbastos@gmail.com>
2026-06-16 13:56:36 -0300

    config para desenvolvimento local

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  allowedDevOrigins: ["*.dev.apps.localhost"],
 };
 
 export default nextConfig;
